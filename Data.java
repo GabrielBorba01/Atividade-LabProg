c134c55124cceed387ee9046d2c6c0d08efdb1f5
GabrielBorba01 <gabriel.112001@outlook.com>
2024-06-14 19:52:55 -0400

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+
 public class Data {
     private int dia;
     private int mes;
     private int ano;
 
+    Scanner leitor = new Scanner(System.in);
+
+    // Construtor padrão
     public Data(){}
 
+    //setData com validacao de dia, mes e ano.
     public void setData(int dia, int mes, int ano){
+        if (ano < 1){
+            System.out.println("Ano Invalido!");
+            return;
+        }
+        if (mes < 1 || mes > 12){
+            System.out.println("Mes Invalido!");
+        }
 
+    }
+
+    //verificar os dias dos meses
+    private int diasNoMes(int mes, int ano){
+        int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (mes == 2 && (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0))) {
+            return 29;
+        }
+        return dias[mes-1];
     }
 
     public int getDia() {
@@ -33,4 +55,8 @@ public class Data {
         this.ano = ano;
     }
 
+    public String getData(){
+        String a = String.format("%02d/%02d/%04d", this.dia, this.mes, this.ano);
+        return a;
+    }
 }
