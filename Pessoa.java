e5acf65af544bda47f5983119cffb82133e67e45
GabrielBorba01 <gabriel.112001@outlook.com>
2024-06-14 20:38:02 -0400

    Image unused delet

--- Pessoa.java
+++ Pessoa.java
@@ -1,3 +1,10 @@
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String email;
 
+    public Pessoa(String cpf){
+        this.cpf = cpf;
+    }
 }
